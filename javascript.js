e8ff1b968d0aac3ee916f45fa9c1cf1020c6771e
Pranav <63364919+Pranav1326@users.noreply.github.com>
2020-07-09 19:48:43 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+console.log("Hello");
+
+// console.log(document.getElementById("btn").onfocus(
+
+// if(document.getElementById("firstName")){
+//     alert("Please enter your First name.");
+// }
+// if(document.getElementById("lastName")) {
+//     alert("Please enter your Last name.");
+// }
+// if(document.getElementById("mobile")) {
+//     alert("Please enter your mobile number");
+// }
+// if(document.getElementById("mail")) {
+//     alert("Please enter your Mail ID.");
+// }
+// else{
+//     console.log("OK");
+// }
+// ));
